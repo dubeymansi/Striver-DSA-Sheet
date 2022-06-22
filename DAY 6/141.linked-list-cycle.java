18a655c56db3e9a798fc1451fb676f6b74ead65d
Mansi Dubey <mansidubey2001me@gmail.com>
2022-06-22 19:26:59 +0530

    DAY -6 Problem 2

---
+++ DAY 6/141.linked-list-cycle.java
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=141 lang=java
+ *
+ * [141] Linked List Cycle
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null)
+        return false;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null && fast.next.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)
+            return true;
+        }
+        return false;
+    }
+        
+    
+}
+// @lc code=end
+
